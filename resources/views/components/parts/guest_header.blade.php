20c6b4afbff9cc0d5ef2d603540ca38ff4c31a63
Kobayashi-shayou <ykobayashi@minna-systems.co.jp>
2023-05-27 23:42:40 +0900

    feat:userの方は見た目やiconをととのえた

--- resources/views/components/parts/guest_header.blade.php
+++ resources/views/components/parts/guest_header.blade.php
@@ -1,10 +1,15 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-	<div class="container">
+	<div class="container" style="max-width: 100%">
+		{{--  ICON --}}
+		<a href="/" class="me-3">
+			<x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+		</a>
+
 		<a class="navbar-brand" href="{{ url('/') }}">
 			{{ config('app.name', 'Laravel') }}
 		</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-			<span class="navbar-toggler-icon"></span>
+		
 		</button>
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
--- resources/views/components/parts/user_basic_card_layout.blade.php
+++ resources/views/components/parts/user_basic_card_layout.blade.php
@@ -1,5 +1,5 @@
-<div class="card shadow-sm">
-    <div class="card-header bg-info text-white d-flex justify-content-between align-items-center">
+<div class="card shadow-sm mt-5">
+    <div class="card-header d-flex justify-content-between align-items-center">
         {{ $cardHeader }}
     </div>
     <div class="card-body">
