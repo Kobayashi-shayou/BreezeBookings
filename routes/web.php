b5046f5a4ad1a1fe2a0e123078e7756fc88c9a4d
Kobayashi-shayou <ykobayashi@minna-systems.co.jp>
2023-05-28 11:02:57 +0900

    feat:routeに開発中ログインを追加

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// 開発中ログイン(ユーザー)
+Route::get('user_dev_login', function () {
+    abort_unless(app()->environment('local'), 403);
+    auth()->guard('user')->login(App\Models\User::first());
+    return to_route('user.home');
+})->name('user_dev_login');
+
+// 開発中ログイン(管理者)
+Route::get('admin_dev_login', function () {
+    abort_unless(app()->environment('local'), 403);
+    auth()->guard('admin')->login(App\Models\Admin::first());
+    return to_route('admin.home');
+})->name('admin_dev_login');
+
+Route::get('admin_dev_login_id/{id}', function ($id) {
+    abort_unless(app()->environment('local'), 403);
+    auth()->guard('admin')->login(App\Models\Admin::find($id));
+    return to_route('admin.home');
+})->name('admin_dev_login_id');
+
+// エラー画面テスト
+Route::get('500', function () {
+    \Log::error('500エラーのテスト');
+    abort(500);
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('top');
 });
 
 Route::get('/dashboard', function () {
@@ -30,14 +56,11 @@ Route::get('/home', function () {
     return view('home');
 })->name('home');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    
-
-
-});
+// });
 
 require __DIR__ . '/auth.php';
