ca382d99a75ba25ae613a39bbfd68612d15b180c
Kobayashi-shayou <ykobayashi@minna-systems.co.jp>
2023-06-01 15:23:36 +0900

    fix:roomテーブル変更

--- database/migrations/2023_05_27_171806_create_rooms_table.php
+++ database/migrations/2023_05_27_171806_create_rooms_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('rooms', function (Blueprint $table) {
             // 料金設定は一部屋いくら（一人でも二人でも同じ）
             $table->id();
+            $table->integer('room_no')->comment('部屋番号');
             $table->string('name')->comment('部屋の名前');
             $table->integer('count')->comment('部屋に泊まれる人数');
             $table->integer('price');
