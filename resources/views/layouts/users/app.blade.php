0f8a9ba522a41f2c541a4caeaea88b139b056ce8
Kobayashi-shayou <ykobayashi@minna-systems.co.jp>
2023-06-01 18:54:15 +0900

    fix:bootstrpを有意に働かせるためにvite以下に配置

--- resources/views/layouts/users/app.blade.php
+++ resources/views/layouts/users/app.blade.php
@@ -11,12 +11,13 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
         <!-- Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
         <body class="font-sans antialiased">
         {{-- フラッシュメッセージ --}}
