b4730f12d593090160c58a43b98cdcc6af49067e
Kobayashi-shayou <ykobayashi@minna-systems.co.jp>
2023-06-09 16:59:18 +0900

    feat:Models関連

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -5,12 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Room extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $guarded = [];
+
+    protected $table = 'm_rooms';
 
     /**
      * The attributes that should be cast.
@@ -20,4 +23,10 @@ class Room extends Model
     protected $casts = [
 
     ];
+
+    // roomと予約枠のリレーション
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(Booking::class, 'room_id', 'id');
+    }
 }
